52b5cd8d5531a34aaf4f71868a3fd00f567489b6
JK <kntrvch@gmail.com>
2025-01-04 02:29:12 +0100

    snackbar info

--- client/src/app/components/employee-details-page/employee-details-page.component.ts
+++ client/src/app/components/employee-details-page/employee-details-page.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { updateEmployee } from '../../store/employees/employees.actions';
 import { Employee } from '../../models/employee';
@@ -16,6 +16,7 @@ import {
 } from '../employee-offboard-dialog/employee-offboard-dialog.component';
 import { EmployeeService } from '../../services/employee.service';
 import { Update } from '@ngrx/entity';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-employee-details-page',
@@ -32,15 +33,18 @@ import { Update } from '@ngrx/entity';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeDetailsPageComponent {
-  private id: string;
   readonly dialog = inject(MatDialog);
+
+  private id: string;
+  private _snackBar = inject(MatSnackBar);
 
   employee$: Observable<Employee | null> | undefined;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private store: Store,
-    private employeeService: EmployeeService
+    private employeeService: EmployeeService,
+    private router: Router
   ) {
     this.id = this.activatedRoute.snapshot.params['id'];
   }
@@ -60,6 +64,8 @@ export class EmployeeDetailsPageComponent {
     dialogRef.afterClosed().subscribe((result) => {
       if (result !== undefined && result.id === this.id) {
         this.updateEmployee();
+        this.openSnackBar('User updated!');
+        this.router.navigate(['/employee-list']);
       }
     });
   }
@@ -71,5 +77,11 @@ export class EmployeeDetailsPageComponent {
     };
 
     this.store.dispatch(updateEmployee({ update }));
+  }
+
+  openSnackBar(message: string) {
+    this._snackBar.open(message, '', {
+      duration: 3000
+    });
   }
 }
